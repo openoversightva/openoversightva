2fda6ceef520060d3044813475eac617f034bfda
Kevin Finity <5213606+kfinity@users.noreply.github.com>
2023-12-10 14:01:05 -0500

    Fix - keep selected job/unit on assignment edit

--- OpenOversight/app/static/js/add_assignment.js
+++ OpenOversight/app/static/js/add_assignment.js
@@ -1,6 +1,8 @@
 function set_jobs() {
   var dept_id = $('#dept').val();
   // fetch jobs for dept_id and modify #job_ids <select>
+  var sel_job = $('#job_title').val();
+  var sel_job_exists = false;
   var jobs_url = $('#add-assignment-form').data('jobs-url');
   var jobs = $.ajax({
     url: jobs_url,
@@ -11,8 +13,12 @@ function set_jobs() {
       $('select#job_title').append(
         $('<option></option>').attr("value", jobs[i][0]).text(jobs[i][1])
       );
+      if (jobs[i][0]==sel_job) { sel_job_exists = true }
     }
+    if (sel_job_exists == true) { $('#job_title').val(sel_job) } // if possible keep selected job
   });
+  var sel_unit = $('#unit').val();
+  var sel_unit_exists = false;
   var units_url = $('#add-assignment-form').data('units-url');
   var units = $.ajax({
     url: units_url,
@@ -23,7 +29,9 @@ function set_jobs() {
       $('select#unit').append(
         $('<option></option>').attr("value", units[i][0]).text(units[i][1])
       );
+      if (units[i][0]==sel_unit) { sel_unit_exists = true }
     }
+    if (sel_unit_exists == true) { $('#unit').val(sel_unit) } // if possible keep selected unit
   });
 }
 
